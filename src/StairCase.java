ba2f2ef56e5f6d63c824b8184cc60bd960722842
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2020-11-03 08:33:39 +0100

    built small staircase

---
+++ src/StairCase.java
@@ -0,0 +1,20 @@
+public class StairCase {
+
+//    public static void printStairs(int n){
+//
+//
+//    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 6; i++){
+            for (int k = 6; k > i; k--){
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
